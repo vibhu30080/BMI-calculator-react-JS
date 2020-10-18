e74ed6fc40ef07eb8b0a87c2083972ad83ad97d9
Khanna <vibkhanna@deloitte.com>
2020-10-18 14:56:42 +0530

    Updates

---
+++ precache-manifest.2cd659864048ffa0043588485004913f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ce1642c6ea40fd1087b180e39eab057",
+    "url": "/BMI-calculator-react-JS/index.html"
+  },
+  {
+    "revision": "64acbfad16a6dafc0a5b",
+    "url": "/BMI-calculator-react-JS/static/css/2.5c17b9f1.chunk.css"
+  },
+  {
+    "revision": "f4a2ea33ca5454a46fda",
+    "url": "/BMI-calculator-react-JS/static/css/main.58392af3.chunk.css"
+  },
+  {
+    "revision": "64acbfad16a6dafc0a5b",
+    "url": "/BMI-calculator-react-JS/static/js/2.574566f7.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/BMI-calculator-react-JS/static/js/2.574566f7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f4a2ea33ca5454a46fda",
+    "url": "/BMI-calculator-react-JS/static/js/main.fef041fd.chunk.js"
+  },
+  {
+    "revision": "17e6965aeb71a65e27e9",
+    "url": "/BMI-calculator-react-JS/static/js/runtime-main.8c70035c.js"
+  }
+]);
